a542d1fe20d86f930856f7cad51f03ef86623f03
Peter <peter.palagyi@gmail.com>
2022-03-03 17:56:13 +0100

    initial commit

---
+++ src/test/java/sk/pgyi/gardening/GardeningApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.pgyi.gardening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardeningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
